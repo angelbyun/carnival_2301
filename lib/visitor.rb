43a1163a386ddadc84f87384067290a43bf7db26
Angel Byun <abyun22@turing.edu>
2023-02-27 09:17:37 -0700

    Feat/Test: Added visitor_spec.rb; created initial commit with attributes for visitor, preferences for visitor, and to check if visitor is tall enough; TDD done for each method to make sure methods are properly functioning

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,26 @@
 class Visitor
-  
+  attr_reader :name,
+              :height,
+              :spending_money,
+              :preferences
+              :tall_enough
+
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = spending_money
+    @preferences = []
+    @tall_enough = true
+  end
+
+  def add_preference(preference)
+    @preferences << preference
+  end
+
+  def tall_enough?
+    if height >= 54
+      @tall_enough
+    else @tall_enough == false
+    end
+  end
 end
---
+++ spec/visitor_spec.rb
@@ -0,0 +1,37 @@
+require './lib/visitor'
+
+RSpec.describe Visitor do
+  it 'exists' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+
+    expect(visitor1).to be_an_instance_of(Visitor)
+    expect(visitor1.name).to eq('Bruce')
+    expect(visitor1.height).to eq(54)
+    expect(visitor1.spending_money).to eq('$10')
+  end
+
+  it 'can list an empty array of preferences' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    
+    expect(visitor1.preferences).to eq([])
+  end
+
+  it 'adds preference' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+
+    visitor1.add_preference(:gentle)
+    visitor1.add_preference(:water)
+
+    expect(visitor1.preferences).to eq([:gentle, :water])
+  end
+
+  it 'can check if visitor is tall enough' do
+    visitor1 = Visitor.new('Bruce', 54, '$10')
+    visitor2 = Visitor.new('Tucker', 36, '$5')
+    visitor3 = Visitor.new('Penny', 64, '$15')
+
+    expect(visitor1.tall_enough?).to eq(true)
+    expect(visitor2.tall_enough?).to eq(false)
+    expect(visitor3.tall_enough?).to eq(true)
+  end
+end
